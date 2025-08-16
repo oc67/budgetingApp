08746a1da6edee659a8da23c680d53d9dbaaad15
unscramblit <unscramblit@gmail.com>
2025-08-16 11:17:28 +0100

    tests added to home and about pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,19 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model=CustomUser
-        fields=UserCreationForm.Meta.fields+("name","surname","age",)
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model=CustomUser
-        fields=UserChangeForm.Meta.fields
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
