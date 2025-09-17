3542aef07fdffc727ce723ec5f45da34e0f7012c
oc67 <oriol67businessl@gmail.com>
2025-09-17 20:29:23 +0100

    fixed serializers error

--- apis/urls.py
+++ apis/urls.py
@@ -6,6 +6,6 @@ from .views import BudgetAPIList, BudgetAPIDetail
 urlpatterns=[
 
     path("",BudgetAPIList.as_view(),name="API_budget_list"),
-    path("<int:pk>",BudgetAPIDetail.as_view(),name="API_budget_detail"),
+    path("<int:pk>/",BudgetAPIDetail.as_view(),name="API_budget_detail"),
 
 ]
