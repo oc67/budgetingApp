ae518aba62ea9be5f50c38546665d9fc93b3fbd8
oc67 <oriol67businessl@gmail.com>
2025-10-05 01:36:04 +0100

    amended payments feature

--- payments/urls.py
+++ payments/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from .views import PaymentHomeView, stripe_config, create_checkout_session
+from .views import PaymentHomeView, SuccessView, CancelView
 from . import views
 
 
 urlpatterns=[
     path("",PaymentHomeView.as_view(), name="payments"),
-    path('config/', stripe_config, name="stripe_config"), 
-    path('create-checkout-session/', create_checkout_session, name="create_checkout_session"), 
 
-    path('success/', views.success, name='success'),
-    path('cancelled/', views.cancelled, name='cancelled'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('cancelled/', CancelView.as_view(), name='cancel'),
 
 
 
--- payments/views.py
+++ payments/views.py
@@ -2,56 +2,56 @@ from django.views.generic.base import TemplateView
 from django.conf import settings 
 from django.http.response import JsonResponse 
 from django.views.decorators.csrf import csrf_exempt 
+from django.urls import reverse  # new
+from django.shortcuts import render, redirect  # new
+
 import stripe
 
-class PaymentHomeView(TemplateView):
-    template_name = 'payments/payments.html'
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-@csrf_exempt
-def stripe_config(request):
-    if request.method == 'GET':
-        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
-        return JsonResponse(stripe_config, safe=False)
-    
+from django.views.generic import (View, TemplateView)
 
 
-@csrf_exempt
-def create_checkout_session(request):
-    if request.method == 'GET':
-        domain_url = 'http://localhost:8000/'
+# products/views.py
+from django.views import View
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.urls import reverse
+import stripe
+
+
+class PaymentHomeView(View):
+    template_name = "payments/payments.html"
+
+    def get(self, request, *args, **kwargs):
+        """Handles GET requests - simply render the home page."""
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        """Handles POST requests - create Stripe checkout session."""
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            # Create new Checkout Session for the order
-            # Other optional params include:
-            # [billing_address_collection] - to display billing address details on the page
-            # [customer] - if you have an existing Stripe Customer ID
-            # [payment_intent_data] - capture the payment later
-            # [customer_email] - prefill the email input in the form
-            # For full details see https://stripe.com/docs/api/checkout/sessions/create
 
-            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-            checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
-                payment_method_types=['card'],
-                mode='payment',
-                line_items=[
-                    {
-                        'name': 'T-shirt',
-                        'quantity': 1,
-                        'currency': 'usd',
-                        'amount': '2000',
-                    }
-                ]
-            )
-            return JsonResponse({'sessionId': checkout_session['id']})
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    "price": "price_1SCMJIHNGjkTZcPNwlq80sDL",  # Replace with your price ID
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url=request.build_absolute_uri(reverse("success")),
+            cancel_url=request.build_absolute_uri(reverse("cancel")),
+        )
 
-from django.shortcuts import render
+        return redirect(checkout_session.url, code=303)
 
-def success(request):
-    return render(request, "payments/success.html")
 
-def cancelled(request):
-    return render(request, "payments/cancelled.html")
+class SuccessView(TemplateView):
+    template_name = "payments/success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "payments/cancelled.html"
+
+
