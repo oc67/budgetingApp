eabf43343ed81c881154c498dbab497f9ee7efe4
oc67 <oriol67businessl@gmail.com>
2025-10-05 13:37:59 +0100

    changed custom user

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,6 @@ class CustomUser(AbstractUser):
     name=models.CharField(max_length=20,null=True,blank=True)
     surname=models.CharField(max_length=20,null=True,blank=True)
     age=models.IntegerField(null=True,blank=True)
+    email=models.EmailField(null=True,blank=True)
+    first_time_on_platform=models.BooleanField(null=True,blank=True,default=True)
+    is_up_to_date_with_payments=models.BooleanField(null=True,blank=True,default=True)
